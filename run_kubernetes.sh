ebf040ee5ceb285b23642589f658e63d69cb5b5f
Ubuntu <ubuntu@ip-172-31-87-245.ec2.internal>
2023-07-12 18:54:09 +0000

    udpate kubernets

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath="nyubblue/ybun-python-app"
+dockerpath="nyubblue/ybun-python-app";
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deploy py-helloworld-kube --image="${dockerpath}:v1.0.0" --port=8000
+kubectl create deploy py-helloworld-kube --image="${dockerpath}:v1.0.0" --port=8000;
 
 # Step 3:
 # List kubernetes pods
-kubectl get pods
+kubectl get pods;
 
 # Step 4:
 # Forward the container port to a host
-POD_NAME=$(kubectl get pods -l app=py-helloworld-kube -o jsonpath='{.items[0].metadata.name}')
+POD_NAME=$(kubectl get pods -l app=py-helloworld-kube -o jsonpath='{.items[0].metadata.name}');
 kubectl port-forward "pod/${POD_NAME}" --address 0.0.0.0 8000:80
